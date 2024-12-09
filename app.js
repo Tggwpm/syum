41c4dc46a12bbb3c172e4116ecb83675fddb7005
Tggwpm <24hs112@s.bgu.ac.jp>
2024-12-09 13:19:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-function checkFortune() {
-            var colour = document.getElementById('colour-select').value;
+   function checkFortune() {
+            var food = document.getElementById('colour-select').value;
             var number = parseInt(document.getElementById('number-input').value, 10);
             var result = "凶"; // デフォルトの値
 
-            if (colour === "黒" && number === 0) {
-                result = "小吉";
-            } else if (colour === "黒" && number === 1) {
-                result = "中吉";
-            } else if (colour === "黒" && number === 2) {
-                result = "凶";
-            } else if (colour === "白" && number === 0) {
-                result = "中吉";
-            } else if (colour === "白" && number === 1) {
-                result = "中吉";
-            } else if (colour === "白" && number === 2) {
-                result = "大凶";
-            } else if (colour === "オレンジ" && number === 0) {
-                result = "凶";
-            } else if (colour === "オレンジ" && number === 1) {
-                result = "大吉";
-            } else if (colour === "オレンジ" && number === 2) {
-                result = "中吉";
+            if (food === "チョコレートアイス" && colour === 赤) {
+                result = "チンチラ";
+            } else if (food === "チョコレートアイス" && colour === 青) {
+                result = "ムササビ";
+            } else if (food === "チョコレートアイス" && colour === 黄色) {
+                result = "カワウソ";
+            } else if (food === "バニラアイス" && colour === 赤) {
+                result = "ポメラニアン";
+            } else if (food === "バニラアイス" && colour === 青) {
+                result = "サル";
+            } else if (food === "バニラアイス" && colour === 黄色) {
+                result = "ハリネズミ";
+            } else if (food === "抹茶アイス" && colour === 赤) {
+                result = "ハムスター";
+            } else if (food === "抹茶アイス" && colour === 青) {
+                result = "ヘビ";
+            } else if (food === "抹茶アイス" && colour === 黄色) {
+                result = "インコ";
             }
 
-            document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
+            document.getElementById('result-output').innerText = "今日のあなたにおすすめの動物の動画は【" + result + "】です";
         }
