d12359a6040e2017c0d0f2a22a5389a9b64045dc
Tggwpm <24hs112@s.bgu.ac.jp>
2024-12-09 14:09:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,54 +1,33 @@
    function checkFortune() {
             var food = document.getElementById('ice-select').value;
-            var number = parseInt(document.getElementById('number-input').value, 30);
+            var number = parseInt(document.getElementById('number-input').value, 10);
             var result = "凶"; // デフォルトの値
 
             if (food === "チョコレートアイス" && number === 1) {
                 result = "チンチラ";
                
             } else if (food === "チョコレートアイス" && number === 2) {
-                result = "ムササビ";
-               
-            } else if (food === "チョコレートアイス" && number === 3) {
                 result = "カワウソ";
                
-            } else if (food === "チョコレートアイス" && number === 4) {
+            } else if (food === "チョコレートアイス" && number === 3) {
                 result = "ポメラニアン";
                
-            } else if (food === "バニラアイス" && number === 1) {
-                result = "サル";
-               
-            } else if (food === "バニラアイス" && number === 2) {
-                result = "ハリネズミ";
-               
-            } else if (food === "バニラアイス" && number === 3) {
-                result = "ハムスター";
-               
-            } else if (food === "バニラアイス" && number === 4) {
-                result = "ヘビ";
-               
-            } else if (food === "ストロベリーアイス" && number === 1) {
-                result = "インコ";
-               
-      　　　} else if (food=== "ストロベリーアイス" && number === 2) {
+      　　　} else if (food=== "ストロベリーアイス" && number === 1) {
                 result = "トラ";
                
-      　　　} else if (food === "ストロベリーアイス" && number === 3) {
+      　　　} else if (food === "ストロベリーアイス" && number === 2) {
                 result = "カバ";
                
-      　　　} else if (food === "ストロベリーアイス" && number === 4) {
-                result = "ペンギン";
-               
-      　　　} else if (food === "ブルーベリーアイス" && number === 1) {
+      　　　} else if (food === "ストロベリーアイス" && number === 3) {
                 result = "シロクマ";
                
-      　　　} else if (food === "ブルーベリーアイス" && number === 2) {
-                result = "アザラシ";
+      　　　} else if (food === "ブルーベリーアイス" && number === 1) {
+                result = "ハムスター";
 
-      　　　} else if (food === "ブルーベリーアイス" && number === 3) {
+      　　　} else if (food === "ブルーベリーアイス" && number === 2) {
                 result = "キツネ";
 
-      　　　} else if (food === "ブルーベリーアイス" && number === 4) {
+      　　　} else if (food === "ブルーベリーアイス" && number === 3) {
                 result = "パンダ";
       
             document.getElementById('result-output').innerText = "今日のあなたにおすすめの動物の動画は【" + result + "】です";
