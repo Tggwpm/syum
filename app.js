1d51dfae8a2bee3421cb8d53a903f45ae3173af4
Tggwpm <24hs112@s.bgu.ac.jp>
2024-12-09 13:53:22 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,52 +3,52 @@
             var number = parseInt(document.getElementById('number-input').value, 10);
             var result = "凶"; // デフォルトの値
 
-            if (food === "キャラメルリボン" && number === 1) {
+            if (ice === "キャラメルリボン" && number === 1) {
                 result = "チンチラ";
                
-            } else if (food === "キャラメルリボン" && number === 2) {
+            } else if (ice === "キャラメルリボン" && number === 2) {
                 result = "ムササビ";
                
-            } else if (food === "キャラメルリボン" && number === 3) {
+            } else if (ice === "キャラメルリボン" && number === 3) {
                 result = "カワウソ";
                
-            } else if (food === "キャラメルリボン" && number === 4) {
+            } else if (ice === "キャラメルリボン" && number === 4) {
                 result = "ポメラニアン";
                
-            } else if (food === "コットンキャンディ" && number === 1) {
+            } else if (ice === "コットンキャンディ" && number === 1) {
                 result = "サル";
                
-            } else if (food === "コットンキャンディ" && number === 2) {
+            } else if (ice === "コットンキャンディ" && number === 2) {
                 result = "ハリネズミ";
                
-            } else if (food === "コットンキャンディ" && number === 3) {
+            } else if (ice === "コットンキャンディ" && number === 3) {
                 result = "ハムスター";
                
-            } else if (food === "コットンキャンディ" && number === 4) {
+            } else if (ice === "コットンキャンディ" && number === 4) {
                 result = "ヘビ";
                
-            } else if (food === "いちごみるく" && number === 1) {
+            } else if (ice === "いちごみるく" && number === 1) {
                 result = "インコ";
                
-      　　　} else if (food === "いちごみるく" && number === 2) {
+      　　　} else if (ice === "いちごみるく" && number === 2) {
                 result = "トラ";
                
-      　　　} else if (food === "いちごみるく" && number === 3) {
+      　　　} else if (ice === "いちごみるく" && number === 3) {
                 result = "カバ";
                
-      　　　} else if (food === "いちごみるく" && number === 4) {
+      　　　} else if (ice === "いちごみるく" && number === 4) {
                 result = "ペンギン";
                
-      　　　} else if (food === "ポッピングシャワー" && number === 1) {
+      　　　} else if (ice === "ポッピングシャワー" && number === 1) {
                 result = "シロクマ";
                
-      　　　} else if (food === "ポッピングシャワー" && number === 2) {
+      　　　} else if (ice === "ポッピングシャワー" && number === 2) {
                 result = "アザラシ";
 
-      　　　} else if (food === "ポッピングシャワー" && number === 3) {
+      　　　} else if (ice === "ポッピングシャワー" && number === 3) {
                 result = "キツネ";
 
-      　　　} else if (food === "ポッピングシャワー" && number === 4) {
+      　　　} else if (ice === "ポッピングシャワー" && number === 4) {
                 result = "パンダ";
       
             document.getElementById('result-output').innerText = "今日のあなたにおすすめの動物の動画は【" + result + "】です";
